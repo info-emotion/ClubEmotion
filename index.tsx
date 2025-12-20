8926e99634c08c46171f3f210a7419435f296f3a
info-emotion <info@porsche-emotion.it>
2025-12-20 18:34:40 +0100

    feat: Integrate Babel Standalone for dynamic script loading
    Replaced the manual `window.process` polyfill with Babel Standalone. This enables dynamic compilation of scripts in the browser, simplifying the development setup and allowing for easier integration of future ES module features.
    
    Also updated the loader animation to use a more standard easing function for a smoother visual experience.

--- index.tsx
+++ index.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const startApp = () => {
+    const rootElement = document.getElementById('root');
+    if (rootElement) {
+        const root = ReactDOM.createRoot(rootElement);
+        root.render(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        );
+        console.log("React Mounted");
+    }
+};
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+startApp();
